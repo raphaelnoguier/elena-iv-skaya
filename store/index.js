345a2212eadb5e7ee199160f71bd954dabd19731
Raphael Ait-el-alim <raphael.ait-el-alim@raitelalim.local>
2019-03-05 11:50:13 +0100

    remove process check in store

--- store/index.js
+++ store/index.js
@@ -18,17 +18,15 @@ export const mutations = {
 
 export const actions = {
   async nuxtServerInit({ commit, error }) {
-    if (process.server) {
-      let path = this.$router.currentRoute.path
-      let route = this.$router.currentRoute.name
-      let isSerie = path.includes('serie');
-      let serieRoute = this.$router.currentRoute.path.replace('/serie/', '');
-      let document = await this.$prismic.api.getByUID(isSerie ? 'serie' : 'page', isSerie ? serieRoute : route);
-      if (document) {
-        commit('SET_DOC', document)
-      } else {
-        error({ statusCode: 404, message: "Page not found" });
-      }
+    let path = this.$router.currentRoute.path
+    let route = this.$router.currentRoute.name
+    let isSerie = path.includes('serie');
+    let serieRoute = this.$router.currentRoute.path.replace('/serie/', '');
+    let document = await this.$prismic.api.getByUID(isSerie ? 'serie' : 'page', isSerie ? serieRoute : route);
+    if (document) {
+      commit('SET_DOC', document)
+    } else {
+      error({ statusCode: 404, message: "Page not found" });
     }
   }
 }
