b8e127251a5ff78a111d2e852f1e429e14a3276f
Raphael Ait-el-alim <raphael.ait-el-alim@raitelalim.local>
2019-03-16 19:45:43 +0100

    fix data getter about

--- pages/About.vue
+++ pages/About.vue
@@ -2,30 +2,30 @@
   <section class="page about">
     <div class="about-wrapper">
       <div class="about-image">
-        <img :src="main_image" />
+        <img :src="doc.main_image.url" />
         <div class="about-quote">
-          <h3>{{title}}</h3>
+          <h3>{{doc.title[0].text}}</h3>
         </div>
       </div>
       <div class="about-content">
         <div class="about-right">
           <div class="social-links">
             <ul>
-              <li v-for="(item, index) in social_links" :key="index">
+              <li v-for="(item, index) in doc.social_links" :key="index">
                 <a :href="item.url">{{item.link_text}}</a>
               </li>
             </ul>
           </div>
           <div class="content-text">
             <div class="paragraph">
-              <p>{{first_paragraph}}</p>
+              <p>{{doc.first_paragraph}}</p>
             </div>
             <div class="two-col">
               <div class="text">
-                <p>{{column_1}}</p>
+                <p>{{doc.column_1}}</p>
               </div>
               <div class="text">
-                <p>{{column_2}}</p>
+                <p>{{doc.column_2}}</p>
               </div>
             </div>
           </div>
@@ -35,7 +35,7 @@
         <div class="agency-representations">
           <span>Agency representations</span>
           <ul>
-            <li v-for="(item, index) in agency_representations" :key="index">
+            <li v-for="(item, index) in doc.agency_representations" :key="index">
               {{item.region}} - <a :href="item.url">{{item.link_text}}</a>
             </li>
           </ul>
@@ -43,7 +43,7 @@
         <div class="credits">
           <span>Website credits</span>
           <ul>
-            <li v-for="(item, index) in credits" :key="index">
+            <li v-for="(item, index) in doc.credits" :key="index">
               {{item.role}} - <a :href="item.url">{{item.link_text}}</a>
             </li>
           </ul>
@@ -54,23 +54,20 @@
 </template>
 <script>
 export default {
-  asyncData ({ app, params, error, store}) {
+  data(){
+    return {
+      doc: []
+    }
+  },
+  async fetch ({ app, params, error, store}) {
     try {
-      let entry = store.dispatch('GET_DOC', app.context.route);
-      let data = entry.data;
-      return {
-        main_image: data.main_image.url,
-        title: data.title[0].text,
-        social_links: data.social_links,
-        first_paragraph: data.first_paragraph[0].text,
-        column_1: data.column_1[0].text,
-        column_2: data.column_2[0].text,
-        agency_representations: data.agency_representations,
-        credits: data.credits
-      }
+      let entry = await store.dispatch('GET_DOC', app.context.route);
     } catch (err) {
       error({statusCode: 404, message: `The page you are looking for does not exist. `, err: err})
     }
   },
+  created() {
+    this.doc = this.$store.getters.currentDoc.data;
+  }
 };
 </script>
