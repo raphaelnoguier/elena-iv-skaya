d6ea62d6f2823a4fac6bd83ed42a90130620ae73
Raphael Ait-el-alim <raphael.ait-el-alim@raitelalim.local>
2019-03-16 14:30:36 +0100

    fix dots on homeslider

--- components/HomeHeader.vue
+++ components/HomeHeader.vue
@@ -7,7 +7,7 @@
         <div class="left"><img src="~assets/img/ui/arrow.svg"><span>previous</span></div>
         <div class="dots">
           <ul>
-            <li v-for="(serie, index) in series" :key="index" :class="index + 1  === 1 ? 'active': ' '">•</li>
+            <li v-for="(serie, index) in series" :key="index" :class="index + 1  === 1 ? 'active': ''">•</li>
           </ul>
         </div>
         <div v-on:click="nextSlide" class="right"><span>next</span><img src="~assets/img/ui/arrow.svg"></div>
