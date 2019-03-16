52d2a17dd3ebb9634801f8c070c331e4bd21ca93
Raphael Ait-el-alim <raphael.ait-el-alim@raitelalim.local>
2019-03-16 18:28:24 +0100

    temporarly fixed about page

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,23 +47,23 @@ module.exports = {
     }]
   ],
   generate: {
-    routes: function (callback) {
-      Prismic.getApi(prismicConfig.apiEndpoint)
-      .then((api) => {
-        return api.query("");
-      }).then((response) => {
-        let routes = response.results.map((doc) => {
-          if(doc.type === 'page' || doc.type === 'about') {
-            return '/' + doc.uid
-          }
-          else if (doc.type === 'serie') {
-            return '/serie/' + doc.uid
-          }
-        })
-        callback(null, routes)
-      })
-      .catch(callback)
-    }
+    // routes: function (callback) {
+    //   Prismic.getApi(prismicConfig.apiEndpoint)
+    //   .then((api) => {
+    //     return api.query("");
+    //   }).then((response) => {
+    //     let routes = response.results.map((doc) => {
+    //       if(doc.type === 'page' || doc.type === 'about') {
+    //         return '/' + doc.uid
+    //       }
+    //       else if (doc.type === 'serie') {
+    //         return '/serie/' + doc.uid
+    //       }
+    //     })
+    //     callback(null, routes)
+    //   })
+    //   .catch(callback)
+    // }
   },
   /*
   ** Build configuration
