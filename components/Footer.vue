f285f55b4424c4eb50f83913c06a08f5bc8dcc04
Raphael Ait-el-alim <raphael.ait-el-alim@raitelalim.local>
2019-03-18 21:19:06 +0100

    add footer links

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,15 @@
 <template>
  <footer>
   <div class="credits">
-    <p>website by <a href="#">robin noguier</a> & <a href="#">raphael ait-el-alim</a></p>
+    <p>
+      website by
+      <a href="http://robin-noguier.com" target="_blank">
+        robin noguier</a>
+        &
+      <a href="http://raphael-aitelalim.fr" target="_blank">
+        raphael ait-el-alim
+      </a>
+    </p>
   </div>
  </footer>
 </template>
