dcae85fff9ff62a03356c7881b1c385ed0dc7a3d
Raphael Ait-el-alim <raphael.ait-el-alim@raitelalim.local>
2019-04-22 13:45:06 +0200

    parallax bloc and img home

--- components/HomeGallery.vue
+++ components/HomeGallery.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="gallery-wrapper" ref="gallery">
-    <div v-for="(serie, index) in series" :key="index" class="gallery-item" :class="getClass(serie.serie.data.cover_ratio)">
-      <nuxt-link v-on:click.native="updateTransitionImg(serie.serie.data.cover_serie_image.url)" :to="`/serie/${serie.serie.uid}`">
-        <div class="home-gallery-mask left"></div>
-        <img :src="serie.serie.data.cover_serie_image.url" alt="home-gallery-cover" />
-        <div class="home-gallery-mask right"></div>
-        <div class="item-title">
-          <h3>{{serie.serie.data.title[0].text}}</h3>
-          <span>{{serie.serie.data.category}}</span>
-        </div>
-      </nuxt-link>
+    <div v-for="(serie, index) in series" :key="index" class="gallery-item-wrapper" :class="serie.serie.data.cover_ratio.includes('Big') && 'full'">
+      <div class="gallery-item" :class="getClass(serie.serie.data.cover_ratio)">
+        <nuxt-link v-on:click.native="updateTransitionImg(serie.serie.data.cover_serie_image.url)" :to="`/serie/${serie.serie.uid}`">
+          <!-- <div class="home-gallery-mask left"></div> -->
+          <img :src="serie.serie.data.cover_serie_image.url" alt="home-gallery-cover" />
+          <!-- <div class="home-gallery-mask right"></div> -->
+          <div class="item-title">
+            <h3>{{serie.serie.data.title[0].text}}</h3>
+            <span>{{serie.serie.data.category}}</span>
+          </div>
+        </nuxt-link>
+      </div>
     </div>
   </div>
 </template>
@@ -51,32 +53,39 @@ export default {
     initParallax() {
       this.galleryItems.forEach((item, i) => {
         if(i = 0) return
-        this.parallax.push({ offsetTop: item.offsetTop, parallax: item.querySelector('img') })
+        this.parallax.push({ offsetTop: item.parentNode.offsetTop, bloc: item, img: item.querySelector('img') })
       });
-    },
-    transform(item, y) {
-      item.style.transform = `translate3d(0, ${y}px, 0) scale3d(1.1, 1.1, 1)`
-    },
-    resetParallax() {
-      for (let i = 0; i < this.parallax.length; i++) {
-        const item = this.parallax[i].parallax
-        item.style.transform = 'translate3d(0, 0px, 0) scale3d(1.1, 1.1, 1)'
-      }
+
+      console.log(this.parallax)
     },
     tick() {
       const wHeight =  window.innerHeight
 
       for (let i = 0; i < this.parallax.length; i++) {
-        const item = this.parallax[i].parallax
+        const item = this.parallax[i].img
+        const bloc = this.parallax[i].bloc
         const offsetTop = -this.offsetY + this.parallax[i].offsetTop
 
         const min = (-wHeight * 0.5) - wHeight * 0.75
         const max = wHeight + (wHeight * 0.75)
 
         if (offsetTop > min && offsetTop < max) {
-          const y = math.map(offsetTop, min, max, -75, 100)
-          this.transform(item, y)
+          const yImg = math.map(offsetTop, min, max, -50, 50)
+          const yBloc = math.map(offsetTop, min, max, 0, 150)
+          this.transform(item, bloc, yBloc, yImg)
         }
+      }
+    },
+    transform(img, bloc, yBloc, yImg) {
+      img.style.transform = `translate3d(0px, ${yImg}px, 0) scale3d(1.1, 1.15, 1)`
+      bloc.style.transform = `translate3d(0px, ${yBloc}px, 0)`
+    },
+    resetParallax() {
+      for (let i = 0; i < this.parallax.length; i++) {
+        const item = this.parallax[i].parallax
+        const bloc = this.parallax[i].bloc
+        item.style.transform = 'translate3d(0, 0px, 0) scale3d(1.1, 1.15, 1)'
+        bloc.style.transform = 'translate3d(0, 0px, 0)'
       }
     },
     updateTransitionImg(serieCover) {
@@ -94,14 +103,14 @@ export default {
       }
     },
     revealGallery() {
-      let tmp = this.$el.querySelectorAll('.home-gallery-mask');
-      const imgs = Array.from(tmp).map(img => {
-        return {
-          dom: img,
-          ratioIn: 0,
-        }
-      })
-      this.reveal = reveal(imgs)
+      // let tmp = this.$el.querySelectorAll('.home-gallery-mask');
+      // const imgs = Array.from(tmp).map(img => {
+      //   return {
+      //     dom: img,
+      //     ratioIn: 0,
+      //   }
+      // })
+      // this.reveal = reveal(imgs)
     },
   },
   props: { series: Array }
