8a38983b1137de338e9e1b7b2a4cd6db4a67ebd3
Raphael Ait-el-alim <raphael.ait-el-alim@raitelalim.local>
2019-05-28 11:36:58 +0200

    =change colors and fix slider links

--- components/HomeSlider.vue
+++ components/HomeSlider.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="type === 'Big' ? 'big-slider' : 'small-slider'" class="home-slider-wrapper">
       <div class="slide" v-for="(serie, index) in featured" :key="index" :class="index + 1  === 1 ? 'active': ' '" :data-slide="index + 1" v-on:click="click">
-        <nuxt-link :to="/serie/`${serie.uid}`">
+        <nuxt-link :to="`serie/${serie.serie.uid}`">
           <div class="image-mask"></div>
           <div class="image">
             <img :src="type === 'Big' ? serie.serie.data.cover_serie_image.url : serie.stripe.url" data-load="preload" :alt="`home-slider-cover${index}`">
